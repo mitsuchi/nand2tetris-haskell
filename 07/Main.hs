2eb78a6ad0036d576da922ea9b544903987e8f9e
sando <sando@3hands.co.jp>
2020-01-15 18:44:13 +0900

    static に対応

--- 07/Main.hs
+++ 07/Main.hs
@@ -16,13 +16,13 @@ main = do
     let file = args !! 0
     content <- readFile file
     let commands = filter (\line -> (not . null) line && (not . P.comment) line) $ map P.trimSpace $ P.plines content
-    let asm = getAsm commands 0 
+    let asm = getAsm commands 0 (fileName file)
     writeFile (changePostfix file "asm") asm
     return ()
 
-getAsm :: [String] -> Int -> String
-getAsm [] n = ""
-getAsm (command:cs) n = case C.commandType command of
+getAsm :: [String] -> Int -> String -> String
+getAsm [] n file = ""
+getAsm (command:cs) n file = case C.commandType command of
     PUSH_COMMAND -> case segment command of
         "constant" -> "@" ++ value command ++ "\nD=A\n@SP\nA=M\nM=D\n@SP\nM=M+1"
         "local" -> "@LCL\nD=M\n@SP\nA=M\nM=D\n@" ++ value command ++ "\nD=A\n@SP\nA=M\nM=D+M\nA=M\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1"
@@ -31,6 +31,7 @@ getAsm (command:cs) n = case C.commandType command of
         "that" -> "@THAT\nD=M\n@SP\nA=M\nM=D\n@" ++ value command ++ "\nD=A\n@SP\nA=M\nM=D+M\nA=M\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1"        
         "temp" -> "@5\nD=A\n@SP\nA=M\nM=D\n@" ++ value command ++ "\nD=A\n@SP\nA=M\nM=D+M\nA=M\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1"        
         "pointer" -> "@3\nD=A\n@SP\nA=M\nM=D\n@" ++ value command ++ "\nD=A\n@SP\nA=M\nM=D+M\nA=M\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1"        
+        "static" -> "@" ++ file ++ "." ++ value command ++ "\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1"
     POP_COMMAND -> case segment command of
         "local" -> "@LCL\nD=M\n@SP\nA=M\nM=D\n@" ++ value command ++ "\nD=A\n@SP\nA=M\nM=D+M\n@SP\nM=M-1\nA=M\nD=M\n@SP\nM=M+1\nA=M\nA=M\nM=D\n@SP\nM=M-1"
         "argument" -> "@ARG\nD=M\n@SP\nA=M\nM=D\n@" ++ value command ++ "\nD=A\n@SP\nA=M\nM=D+M\n@SP\nM=M-1\nA=M\nD=M\n@SP\nM=M+1\nA=M\nA=M\nM=D\n@SP\nM=M-1"
@@ -38,6 +39,7 @@ getAsm (command:cs) n = case C.commandType command of
         "that" -> "@THAT\nD=M\n@SP\nA=M\nM=D\n@" ++ value command ++ "\nD=A\n@SP\nA=M\nM=D+M\n@SP\nM=M-1\nA=M\nD=M\n@SP\nM=M+1\nA=M\nA=M\nM=D\n@SP\nM=M-1"
         "temp" -> "@5\nD=A\n@SP\nA=M\nM=D\n@" ++ value command ++ "\nD=A\n@SP\nA=M\nM=D+M\n@SP\nM=M-1\nA=M\nD=M\n@SP\nM=M+1\nA=M\nA=M\nM=D\n@SP\nM=M-1"
         "pointer" -> "@3\nD=A\n@SP\nA=M\nM=D\n@" ++ value command ++ "\nD=A\n@SP\nA=M\nM=D+M\n@SP\nM=M-1\nA=M\nD=M\n@SP\nM=M+1\nA=M\nA=M\nM=D\n@SP\nM=M-1"
+        "static" -> "@SP\nM=M-1\nA=M\nD=M\n@" ++ file ++ "." ++ value command ++ "\nM=D"
     CALC_COMMAND -> case operand command of
         "add" -> "@SP\nM=M-1\n@SP\nA=M\nA=M\nD=A\n@SP\nM=M-1\n@SP\nA=M\nM=D+M\n@SP\nM=M+1"
         "sub" -> "@SP\nM=M-1\n@SP\nA=M\nA=M\nD=A\n@SP\nM=M-1\n@SP\nA=M\nM=M-D\n@SP\nM=M+1"
@@ -55,7 +57,10 @@ getAsm (command:cs) n = case C.commandType command of
         "or" -> "@SP\nM=M-1\n@SP\nA=M\nA=M\nD=A\n@SP\nM=M-1\n@SP\nA=M\nM=D|M\n@SP\nM=M+1"
         "not" -> "@SP\nM=M-1\nA=M\nM=!M\n@SP\nM=M+1"
         _     -> "1" ++ operand command ++ "2"
-  ++ "\n" ++ getAsm cs (n+1)
+  ++ "\n" ++ getAsm cs (n+1) file
 
 changePostfix :: String -> String -> String
 changePostfix str postfix = (++ postfix ) . reverse . dropWhile (/= '.') . reverse $ str
+
+fileName :: String -> String
+fileName str = map (\x -> if x == '/' then '-' else x) . takeWhile (/= '.') $ str
